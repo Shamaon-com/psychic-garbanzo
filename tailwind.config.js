b2448f3ab6ab2500a1390b18ce390dd966ae2d42
pabloVoorvaart <pablov@graficadirecta.com>
2021-01-12 19:17:29 +0100

    rework to typescript

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'hero-lg': "url('/img/handshake.jpeg')",
+      },
+    },
   },
   variants: {
     extend: {},
