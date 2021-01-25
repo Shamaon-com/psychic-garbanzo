ade2193651fe9ed5dbc6357bf097bf373a3b90c5
pabloVoorvaart <pablov@graficadirecta.com>
2021-01-25 11:09:38 +0100

    fixed bugs for #7

--- components/questionList.js
+++ components/questionList.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useRef, useContext } from 'react';
-import { API, graphqlOperation } from 'aws-amplify';
+import { a, API, graphqlOperation } from 'aws-amplify';
 import * as mutations from '../config/graphql/mutations';
 import * as queries from '../config/graphql/queries';
 import * as subscriptions from '../config/graphql/subscriptions';
@@ -9,75 +9,63 @@ import useDynamicRefs from 'use-dynamic-refs';
 import Ponentes from '../pages/control/ponentes';
 
 export default function QuestionList({ ...props }) {
+
+	const [array, setArray] = useState([])
 	const [getRef, setRef] = useDynamicRefs();
-	const renderQuestions = () => {
+	const questionsEndRef = useRef(null);
+
+
+	const authContext = useContext(AuthContext);
+
+
+	useEffect(() => {
+		console.log(props);
+		setArray(props.data);
+
+	}, [props.data]);
+
+
 		return (
 			<>
-				{props.questions.map((question, key) => {
-					let classVar =
-						'bg-white text-gray-700 p-2 self-start my-2 rounded-md shadow mr-3';
-					if (
-						props.question.user !==
-						props.authContext.attributes.email
-					) {
-						classVar =
-							'bg-green-500 text-white p-2 self-end my-2 rounded-md shadow ml-3';
-					}
+				{array.map((item, key) => {
+					let classVar = 'bg-white text-gray-700 p-2 self-start my-2 rounded-md shadow mr-3';
 					return (
 						<div
 							key={key}
-							id={question.id}
+							id={item.id}
 							className={classVar}
-							onMouseEnter={props.showDelete}
-							onMouseLeave={props.hideDelete}
 						>
-							{props.authContext.isAdmin && (
-								<svg
-									ref={setRef(props.question.id)}
-									style={{ display: 'none', width: '20px' }}
-									className="ml-auto p-0.5 cursor-pointer"
-									viewBox="0 0 512 512"
-									width="512pt"
-									xmlns="http://www.w3.org/2000/svg"
-									onClick={props.deleteQuestion}
-								>
-									<path
-										d="m256 0c-141.164062 0-256 114.835938-256 256s114.835938 256 256 256 256-114.835938 256-256-114.835938-256-256-256zm0 0"
-										fill="#f44336"
-									/>
-									<path
-										d="m350.273438 320.105469c8.339843 8.34375 8.339843 21.824219 0 30.167969-4.160157 4.160156-9.621094 6.25-15.085938 6.25-5.460938 0-10.921875-2.089844-15.082031-6.25l-64.105469-64.109376-64.105469 64.109376c-4.160156 4.160156-9.621093 6.25-15.082031 6.25-5.464844 0-10.925781-2.089844-15.085938-6.25-8.339843-8.34375-8.339843-21.824219 0-30.167969l64.109376-64.105469-64.109376-64.105469c-8.339843-8.34375-8.339843-21.824219 0-30.167969 8.34375-8.339843 21.824219-8.339843 30.167969 0l64.105469 64.109376 64.105469-64.109376c8.34375-8.339843 21.824219-8.339843 30.167969 0 8.339843 8.34375 8.339843 21.824219 0 30.167969l-64.109376 64.105469zm0 0"
-										fill="#fafafa"
-									/>
-								</svg>
-							)}
+						<svg
+							style={{ width: '20px' }}
+							className="ml-auto p-0.5 cursor-pointer"
+							viewBox="0 0 512 512"
+							width="512pt"
+							xmlns="http://www.w3.org/2000/svg"
+							onClick={props.deleteItem}
+						>
+							<path
+								d="m256 0c-141.164062 0-256 114.835938-256 256s114.835938 256 256 256 256-114.835938 256-256-114.835938-256-256-256zm0 0"
+								fill="#f44336"
+							/>
+							<path
+								d="m350.273438 320.105469c8.339843 8.34375 8.339843 21.824219 0 30.167969-4.160157 4.160156-9.621094 6.25-15.085938 6.25-5.460938 0-10.921875-2.089844-15.082031-6.25l-64.105469-64.109376-64.105469 64.109376c-4.160156 4.160156-9.621093 6.25-15.082031 6.25-5.464844 0-10.925781-2.089844-15.085938-6.25-8.339843-8.34375-8.339843-21.824219 0-30.167969l64.109376-64.105469-64.109376-64.105469c-8.339843-8.34375-8.339843-21.824219 0-30.167969 8.34375-8.339843 21.824219-8.339843 30.167969 0l64.105469 64.109376 64.105469-64.109376c8.34375-8.339843 21.824219-8.339843 30.167969 0 8.339843 8.34375 8.339843 21.824219 0 30.167969l-64.109376 64.105469zm0 0"
+								fill="#fafafa"
+							/>
+						</svg>
+						
 							<div className="text-sm">
-								{question.user.split('@')[0]} -{' '}
+								{item.user.split('@')[0]} -{' '}
 								{new Date(
-									question.createdAt
+									item.createdAt
 								).toLocaleTimeString()}
 							</div>
-							{question.question}
+							{item.question}
 						</div>
 					);
 				})}
-				<div ref={props.questionsEndRef}></div>
 			</>
 		);
-	};
+	
 
-	return (
-		<div className="flex flex-col h-mobile lg:h-full w-full border-8 border-gray-300">
-			<div className="flex justify-between items-center text-white p-1 bg-gray-500 shadow-lg mr-5 w-full">
-				<div className="flex items-center">
-					<h2 className="font-semibold tracking-wider">Preguntas</h2>
-				</div>
-			</div>
-			<div className="flex flex-col bg-gray-200 px-2 overflow-auto h-full">
-				<div className=" flex flex-col mt-auto">
-					{renderQuestions()}
-				</div>
-			</div>
-		</div>
-	);
+
 }
--- pages/control/ponentes.js
+++ pages/control/ponentes.js
@@ -10,10 +10,12 @@ import * as queries from '../../config/graphql/queries';
 import * as subscriptions from '../../config/graphql/subscriptions';
 
 export default function Ponentes() {
-	const [question, setQuestion] = useState('');
+
 	const [questions, setQuestions] = useState([]);
 	const [getRef, setRef] = useDynamicRefs();
+
 	const questionsEndRef = useRef(null);
+
 	const authContext = useContext(AuthContext);
 
 	useEffect(() => {
@@ -86,6 +88,8 @@ export default function Ponentes() {
 			},
 		});
 	};
+
+
 	/**
 	 *
 	 * Utils
@@ -103,114 +107,44 @@ export default function Ponentes() {
 	 * UI Functions
 	 */
 
-	// const scrollToBottom = () => {
-	// 	QuestionList.questionsEndRef.current.scrollIntoView();
-	// };
+	const scrollToBottom = () => {
+	 	QuestionList.questionsEndRef.current.scrollIntoView();
+	 };
 
-	/** 
-  <div class="flex pr-4 ml-auto">
-  <button class="h-10 px-5 m-2 text-indigo-100 transition-colors duration-150 bg-indigo-700 rounded-lg focus:shadow-outline hover:bg-indigo-800">
-    Subir CSV
-  </button>
-</div>
-*/
-	const renderQuestionCell = () => {
-		const keys = ['sub', 'phone_number_verified', 'email_verified'];
+
+	const renderQuestionList = () => {
+		
 
 		return (
 			<tbody>
 				{questions.map((question, key) => {
 					return (
-						<tr key={key}>
-							<td class="border-dashed border-t border-gray-200 px-3">
-								<label class="text-teal-500 inline-flex justify-between items-center hover:bg-gray-200 px-2 py-2 rounded-lg cursor-pointer">
-									<input
-										type="checkbox"
-										class="form-checkbox rowCheckbox focus:outline-none focus:shadow-outline"
-										onClick={(e) => {
-											setIndex(key);
-										}}
-									/>
-								</label>
-							</td>
-							{question.Attributes.map((attribute, key) => {
-								if (!keys.includes(attribute.Name)) {
-									return (
-										<td class="border-dashed border-t border-gray-200">
-											<span class="text-gray-700 px-6 py-3 flex items-center">
-												{attribute.Value}
-											</span>
-										</td>
-									);
-								}
-							})}
-						</tr>
+						<div> 
+							{question.question}
+						</div>
 					);
 				})}
 			</tbody>
 		);
 	};
-	const renderKey = () => {
-		//console.log(userTable.headings)
-		const keys = ['sub', 'phone_number_verified', 'email_verified'];
-		if (questions[0] !== undefined) {
-			return (
-				<>
-					{questions[0].Attributes.map((item, key) => {
-						if (!keys.includes(item.Name)) {
-							return (
-								<th class="bg-gray-100 sticky top-0 border-b border-gray-200 px-6 py-2 text-gray-600 font-bold tracking-wider uppercase text-xs">
-									{item.Name}
-								</th>
-							);
-						}
-					})}
-				</>
-			);
-		}
-	};
+
+
+
 	const renderMain = () => {
+		console.log(questions)
 		return (
-			<>
-				<div class=" w-full space-y-8">
-					<div
-						class="overflow-x-auto bg-white rounded-lg shadow overflow-y-auto relative"
-						style={{ height: '85%' }}
-					>
-						<table class="border-collapse table-auto w-full whitespace-no-wrap bg-white table-striped relative">
-							<thead>
-								<tr class="text-left">
-									<th class="py-2 px-3 sticky top-0 border-b border-gray-200 bg-gray-100">
-										<label class="text-teal-500 inline-flex justify-between items-center hover:bg-gray-200 px-2 py-2 rounded-lg cursor-pointer">
-											<input
-												type="checkbox"
-												class="form-checkbox focus:outline-none focus:shadow-outline"
-											/>
-										</label>
-									</th>
-									{renderKey()}
-								</tr>
-							</thead>
-							{renderQuestionCell()}
-						</table>
-					</div>
+			<div class=" w-full space-y-8">
+				<div
+					class="overflow-x-auto bg-white rounded-lg shadow overflow-y-auto relative"
+					style={{ height: '100%', maxHeight: '85%' }}
+				>
+				<QuestionList data={questions} deleteItem={deleteQuestion}/> 
 				</div>
-				)
-			</>
+			</div>
 		);
 	};
+
+
 	return <AdminLayout>{renderMain()}</AdminLayout>;
-	// return (
-	// 	<AdminLayout>
-	// 		<QuestionList
-	// 			questions={questions}
-	// 			question={question}
-	// 			authContext={authContext}
-	// 			showDelete={showDelete}
-	// 			hideDelete={hideDelete}
-	// 			getRef={getRef}
-	// 			setRef={setRef}
-	// 		/>
-	// 	</AdminLayout>
-	// );
+
 }
